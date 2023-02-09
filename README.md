7765120b2c25e13f35a05ce6078b80a526ed6df8
Geoffrey-Fish <earl@geoffrey.fish>
2023-02-09 14:49:33 +0100

    review

--- README.md
+++
@@ -1,2 +0,0 @@
-# SQLEinstiegBuch
- SQL Learning
--- gitfish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git config user.name "Geoffrey-Fish"
-git config user.email "earl@geoffrey.fish"
-
-#da mir das daurnd wieder zurück gestellt wird, der mist.
